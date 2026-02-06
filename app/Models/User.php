5d545795abe8295d4d5d889abba0dd3d29e88a00
sina hosseini <sina6g@gmail.com>
2026-02-06 23:53:17 +0330

    user profile

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,13 +18,64 @@ class User extends Authenticatable
      * @var list<string>
      */
     protected $fillable = [
-        'full_name',
+        'first_name',
+        'last_name',
         'phone',
         'email',
     ];
 
-    public function company()
-    {
-        return $this->belongsTo(Company::class);
-    }
+    const GENDER_MALE = 0;
+    const GENDER_FEMALE = 1;
+    const GENDERS = [
+        self::GENDER_MALE,
+        self::GENDER_FEMALE,
+    ];
+
+    const FOOT_SPECIALIZATION_RIGHT = 'right';
+    const FOOT_SPECIALIZATION_LEFT = 'left';
+    const FOOT_SPECIALIZATION_BOTH = 'both';
+    const FOOT_SPECIALIZATION = [
+        self::FOOT_SPECIALIZATION_RIGHT,
+        self::FOOT_SPECIALIZATION_LEFT,
+        self::FOOT_SPECIALIZATION_BOTH,
+    ];
+
+    const POST_SKILL_GK = 'GK';
+    const POST_SKILL_CB = 'CB';
+    const POST_SKILL_RB = 'RB';
+    const POST_SKILL_LB = 'LB';
+    const POST_SKILL_DM = 'DM';
+    const POST_SKILL_CM = 'CM';
+    const POST_SKILL_AM = 'AM';
+    const POST_SKILL_LW = 'LW';
+    const POST_SKILL_RW = 'RW';
+    const POST_SKILL_CF = 'CF';
+    const POST_SKILL_SS = 'SS';
+    const POST_SKILL_ST = 'ST';
+
+    const POST_SKILL = [
+        self::POST_SKILL_GK,
+        self::POST_SKILL_CB,
+        self::POST_SKILL_RB,
+        self::POST_SKILL_LB,
+        self::POST_SKILL_DM,
+        self::POST_SKILL_CM,
+        self::POST_SKILL_AM,
+        self::POST_SKILL_LW,
+        self::POST_SKILL_RW,
+        self::POST_SKILL_CF,
+        self::POST_SKILL_SS,
+        self::POST_SKILL_ST
+    ];
+
+
+
+    const SKILL_LEVEL_BEGINNER = 'beginner';
+    const SKILL_LEVEL_SEMI_PROFESSIONAL = 'semi-professional';
+    const SKILL_LEVEL_PROFESSIONAL = 'professional';
+    const SKILL_LEVEL = [
+        self::SKILL_LEVEL_BEGINNER,
+        self::SKILL_LEVEL_SEMI_PROFESSIONAL,
+        self::SKILL_LEVEL_PROFESSIONAL,
+    ];
 }
--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,9 +13,28 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('full_name')->nullable();
+            $table->string('first_name')->nullable();
+            $table->string('last_name')->nullable();
             $table->string('phone')->unique();
             $table->string('email')->unique()->nullable();
+            $table->string('username')->unique()->nullable();
+            $table->unsignedBigInteger('province_id')->nullable();
+            $table->unsignedBigInteger('city_id')->nullable();
+            $table->foreign('province_id')->references('id')->on('provinces');
+            $table->foreign('city_id')->references('id')->on('cities');
+            $table->enum('gender',\App\Models\User::GENDERS)->nullable();
+            $table->string('birth_date')->nullable();
+            $table->integer('weight')->default(0);
+            $table->integer('height')->default(0);
+            $table->enum('foot_specialization',\App\Models\User::FOOT_SPECIALIZATION)->nullable();
+            $table->enum('post_skill',\App\Models\User::POST_SKILL)->nullable();
+            $table->enum('skill_level',\App\Models\User::SKILL_LEVEL)->nullable();
+            $table->boolean('activity_history')->nullable();
+            $table->string('team_name')->nullable();
+            $table->string('favorite_iranian_team')->nullable();
+            $table->string('favorite_foreign_team')->nullable();
+            $table->integer('shirt_number')->nullable();
+            $table->string('bio')->nullable();
             $table->timestamps();
         });
 
